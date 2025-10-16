438b0d5e240822cb422446e32c639d172de682da
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-10-16 09:38:49 -0400

    Update and rename PracticeProblemTest.java to ProblemSetTest.java

---
+++ src/test/java/ProblemSetTest.java
@@ -0,0 +1,12 @@
+import java.lang.reflect.Method;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+public class ProblemSetTest {
+@Test
+@DisplayName("")
+void ExampleTest1() {
+	ArrayEquals("a", "b");
+}
+
+}
